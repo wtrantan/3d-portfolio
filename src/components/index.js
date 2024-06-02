273018b863762007cca35cd4c0a8f3a717ba1bfd
William Trantan <wtrantan@gmail.com>
2024-06-01 20:25:01 -0700

    spaceship part 2

--- src/components/index.js
+++ src/components/index.js
@@ -6,7 +6,7 @@ import Navbar from "./Navbar";
 import Portfolio from "./Portfolio";
 import Position from "./Position";
 import SpacemanCanvas from "./Spaceman";
-import RetroCanvas from "./retro";
+import RetroCanvas from "./Retro.jsx";
 import About from "./About";
 export {
     CanvasLoader, Contact, Experience, Hero, Navbar, Portfolio, Position, SpacemanCanvas, About, RetroCanvas
